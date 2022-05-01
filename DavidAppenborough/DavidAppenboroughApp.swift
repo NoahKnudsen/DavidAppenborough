6a154b29b830559d60af627d11e2b58761685c15
Noah Knudsen <KnudsenNoah@gmail.com>
2022-05-01 11:34:02 +0100

    Initial Commit

---
+++ DavidAppenborough/DavidAppenboroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavidAppenboroughApp.swift
+//  DavidAppenborough
+//
+//  Created by Noah Knudsen on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DavidAppenboroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
